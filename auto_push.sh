2cb4806a48be4d3a751766b943bc45f2a19ad7e1
UtkarshDeoli <utkarsh.deoli@gmail.com>
2024-08-28 18:09:31 +0530

    fix

---
+++ auto_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd ~/Documents/Projects/ccwig
+
+# Check for changes
+if [[ $(git status --porcelain) ]]; then
+  # Add changes to the staging area
+  git add .
+
+  # Commit changes
+  git commit -m "Schedled Commit on $(date)"
+
+  # Push changes to the remote repository
+  git pull
+  git push
+else
+  echo "No changes to commit."
+fi
