8909edd0b63d71480e3335957f919e5e63f0a3bb
UtkarshDeoli <utkarsh.deoli@gmail.com>
2024-11-29 00:03:06 +0530

    added new changes to auto commit

--- auto_push.sh
+++ auto_push.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# Function to read commit message with a timeout
+read_commit_message() {
+  read -t 60 -p "Enter commit message: " commit_message
+  if [ -z "$commit_message" ]; then
+    commit_message="Auto Push with changes $(date)"
+  fi
+}
+
 # Check for changes
 if [[ $(git status --porcelain) ]]; then
   # Add changes to the staging area
   git add .
 
+  # Get commit message
+  read_commit_message
+
   # Commit changes
-  git commit -m "Auto Push with changes $(date)"
+  git commit -m "$commit_message"
 
   # Push changes to the remote repository
   git pull
